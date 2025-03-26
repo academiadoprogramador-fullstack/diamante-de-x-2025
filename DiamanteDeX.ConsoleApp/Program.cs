8ee91dc075dacdd3b82342a8757f5332f30e7443
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-03-26 17:17:04 -0300

    Implementado desesenho da parte superior

--- DiamanteDeX.ConsoleApp/Program.cs
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -6,6 +6,25 @@
         {
             int tamanhoDoDiamante = 5;
 
+            int quantidadeDeLinhas = (tamanhoDoDiamante - 1) / 2;
+            int quantidadeDeEspacos = (tamanhoDoDiamante - 1) / 2;
+            int quantidadeDeX = 1;
+
+            // Desenho da parte superior
+            for (int linha = 0; linha < quantidadeDeLinhas; linha++)
+            {
+                for (int espacos = 0; espacos < quantidadeDeEspacos; espacos++)
+                    Console.Write(" ");
+
+                for (int x = 0; x < quantidadeDeX; x++)
+                    Console.Write("x");
+
+                quantidadeDeEspacos--;
+                quantidadeDeX += 2;
+
+                Console.WriteLine();
+            }
+
             // Desenho da parte do meio
             for (int coluna = 0; coluna < tamanhoDoDiamante; coluna++)
             {

76c46d0a1247c4b6d6beae898c776bc76700fd10
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-03-26 17:18:48 -0300

    Implementado desenho da parte inferior

--- DiamanteDeX.ConsoleApp/Program.cs
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -27,11 +27,27 @@
 
             // Desenho da parte do meio
             for (int coluna = 0; coluna < tamanhoDoDiamante; coluna++)
-            {
                 Console.Write("x");
-            }
 
             Console.WriteLine();
+
+            // Desenho da parte inferior
+            quantidadeDeEspacos = 1;
+            quantidadeDeX -= 2;
+
+            for (int linha = 0; linha < quantidadeDeLinhas; linha++)
+            {
+                for (int espacos = 0; espacos < quantidadeDeEspacos; espacos++)
+                    Console.Write(" ");
+
+                for (int x = 0; x < quantidadeDeX; x++)
+                    Console.Write("x");
+
+                quantidadeDeEspacos++;
+                quantidadeDeX -= 2;
+
+                Console.WriteLine();
+            }
 
             Console.ReadLine();
         }

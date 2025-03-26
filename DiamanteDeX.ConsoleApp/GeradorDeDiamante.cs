d9e7deb1a7f6be4653d9d1be4ddd45a79a3c0ea5
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-03-26 18:21:20 -0300

    Implementada classe GeradorDeDiamante para realização de operações do desenho

---
+++ DiamanteDeX.ConsoleApp/GeradorDeDiamante.cs
@@ -0,0 +1,62 @@
+﻿namespace DiamanteDeX.ConsoleApp;
+
+public static class GeradorDeDiamante
+{
+    public static int QuantidadeDeX;
+    public static int QuantidadeDeEspacos;
+    public static int QuantidadeDeLinhas;
+
+    public static void Iniciar(int tamanhoDoDiamante)
+    {
+        QuantidadeDeX = 1;
+        QuantidadeDeEspacos = CalcularQuantidadeDeEspacos(tamanhoDoDiamante);
+        QuantidadeDeLinhas = CalcularQuantidadeDeLinhas(tamanhoDoDiamante);
+    }
+
+    public static string ObterEspacosDaLinha()
+    {
+        string espacosDaLinha = "";
+
+        for (int espacos = 0; espacos < QuantidadeDeEspacos; espacos++)
+            espacosDaLinha += " ";
+
+        return espacosDaLinha;
+    }
+
+    public static string ObterXsDaLinha()
+    {
+        string xsDaLinha = "";
+
+        for (int x = 0; x < QuantidadeDeX; x++)
+            xsDaLinha += "x";
+
+        return xsDaLinha;
+    }
+
+    public static int CalcularQuantidadeDeLinhas(int tamanhoDoDiamante)
+    {
+        return (tamanhoDoDiamante - 1) / 2;
+    }
+
+    public static int CalcularQuantidadeDeEspacos(int tamanhoDoDiamante)
+    {
+        return (tamanhoDoDiamante - 1) / 2;
+    }
+
+    public static void IncrementarLinha()
+    {
+        QuantidadeDeX += 2;
+        QuantidadeDeEspacos--;
+    }
+
+    public static void DecrementarLinha()
+    {
+        QuantidadeDeX -= 2;
+        QuantidadeDeEspacos++;
+    }
+
+    public static bool TamanhoDoDiamanteEstaValido(int tamanhoDoDiamante)
+    {
+        return tamanhoDoDiamante <= 1 || tamanhoDoDiamante > 99 || tamanhoDoDiamante % 2 == 0;
+    }
+}
--- DiamanteDeX.ConsoleApp/Program.cs
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -1,86 +1,89 @@
-﻿namespace DiamanteDeX.ConsoleApp
+﻿namespace DiamanteDeX.ConsoleApp;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        while (true)
         {
-            while (true)
+            ExibirCabecalho();
+
+            int tamanhoDoDiamante = ObterTamanhoDoDiamante();
+
+            GeradorDeDiamante.Iniciar(tamanhoDoDiamante);
+
+            // Desenho da parte superior
+            for (int linha = 0; linha < GeradorDeDiamante.QuantidadeDeLinhas; linha++)
             {
-                int tamanhoDoDiamante;
-                bool numeroEhValido = false;
+                Console.Write(GeradorDeDiamante.ObterEspacosDaLinha());
+                Console.Write(GeradorDeDiamante.ObterXsDaLinha());
 
-                do
-                {
-                    Console.Clear();
-                    Console.WriteLine("---------------------------------------------");
-                    Console.WriteLine("Diamante de X");
-                    Console.WriteLine("---------------------------------------------");
-
-                    Console.Write("Digite um número ímpar positivo: ");
-                    numeroEhValido = int.TryParse(Console.ReadLine(), out tamanhoDoDiamante);
-
-                    Console.WriteLine("---------------------------------------------");
-
-                    if (tamanhoDoDiamante <= 1 || tamanhoDoDiamante > 99 || tamanhoDoDiamante % 2 == 0)
-                    {
-                        numeroEhValido = false;
-                        Console.WriteLine("Por favor, digite um número ímpar maior que 1 e menor que 100!");
-                        Console.WriteLine("---------------------------------------------");
-
-                        Console.ReadLine();
-                    }
-                } while (numeroEhValido == false);
-
-                int quantidadeDeLinhas = (tamanhoDoDiamante - 1) / 2;
-                int quantidadeDeEspacos = (tamanhoDoDiamante - 1) / 2;
-                int quantidadeDeX = 1;
-
-                // Desenho da parte superior
-                for (int linha = 0; linha < quantidadeDeLinhas; linha++)
-                {
-                    for (int espacos = 0; espacos < quantidadeDeEspacos; espacos++)
-                        Console.Write(" ");
-
-                    for (int x = 0; x < quantidadeDeX; x++)
-                        Console.Write("x");
-
-                    quantidadeDeEspacos--;
-                    quantidadeDeX += 2;
-
-                    Console.WriteLine();
-                }
-
-                // Desenho da parte do meio
-                for (int coluna = 0; coluna < tamanhoDoDiamante; coluna++)
-                    Console.Write("x");
+                GeradorDeDiamante.IncrementarLinha();
 
                 Console.WriteLine();
-
-                // Desenho da parte inferior
-                quantidadeDeEspacos = 1;
-                quantidadeDeX -= 2;
-
-                for (int linha = 0; linha < quantidadeDeLinhas; linha++)
-                {
-                    for (int espacos = 0; espacos < quantidadeDeEspacos; espacos++)
-                        Console.Write(" ");
-
-                    for (int x = 0; x < quantidadeDeX; x++)
-                        Console.Write("x");
-
-                    quantidadeDeEspacos++;
-                    quantidadeDeX -= 2;
-
-                    Console.WriteLine();
-                }
-
-                Console.WriteLine("---------------------------------------------");
-                Console.Write("Deseja desenhar outro diamante? s/N ");
-
-                if (Console.ReadLine()?.ToUpper() != "S")
-                    break;
             }
-            
+
+            // Desenho da parte do meio
+            Console.Write(GeradorDeDiamante.ObterXsDaLinha());
+            Console.WriteLine();
+
+            // Desenho da parte inferior
+            GeradorDeDiamante.QuantidadeDeEspacos = 1;
+            GeradorDeDiamante.QuantidadeDeX -= 2;
+
+            for (int linha = 0; linha < GeradorDeDiamante.QuantidadeDeLinhas; linha++)
+            {
+                Console.Write(GeradorDeDiamante.ObterEspacosDaLinha());
+                Console.Write(GeradorDeDiamante.ObterXsDaLinha());
+
+                GeradorDeDiamante.DecrementarLinha();
+
+                Console.WriteLine();
+            }
+
+            if (UsuarioDesejaSair())
+                break;
         }
+    }
+    static void ExibirCabecalho()
+    {
+        Console.Clear();
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine("Diamante de X");
+        Console.WriteLine("---------------------------------------------");
+    }
+
+    static int ObterTamanhoDoDiamante()
+    {
+        int tamanhoDoDiamante;
+        bool numeroEhValido = false;
+
+        do
+        {
+            Console.Write("Digite um número ímpar positivo: ");
+            numeroEhValido = int.TryParse(Console.ReadLine(), out tamanhoDoDiamante);
+
+            Console.WriteLine("---------------------------------------------");
+
+            if (GeradorDeDiamante.TamanhoDoDiamanteEstaValido(tamanhoDoDiamante))
+            {
+                numeroEhValido = false;
+                Console.WriteLine("Por favor, digite um número ímpar maior que 1 e menor que 100!");
+                Console.WriteLine("---------------------------------------------");
+
+                Console.ReadLine();
+            }
+
+        } while (numeroEhValido == false);
+
+        return tamanhoDoDiamante;
+    }
+
+    static bool UsuarioDesejaSair()
+    {
+        Console.WriteLine("---------------------------------------------");
+        Console.Write("Deseja desenhar outro diamante? s/N ");
+
+        return Console.ReadLine()?.ToUpper() != "S";
     }
 }

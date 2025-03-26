311fd533549a2ad30e3762c6f27cc98ed1d43c8a
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-03-26 17:24:11 -0300

    Implementadas multiplas tentativas

--- DiamanteDeX.ConsoleApp/Program.cs
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -4,75 +4,83 @@
     {
         static void Main(string[] args)
         {
-            int tamanhoDoDiamante;
-            bool numeroEhValido = false;
-
-            do
+            while (true)
             {
-                Console.Clear();
-                Console.WriteLine("---------------------------------------------");
-                Console.WriteLine("Diamante de X");
-                Console.WriteLine("---------------------------------------------");
+                int tamanhoDoDiamante;
+                bool numeroEhValido = false;
 
-                Console.Write("Digite um número impar positivo: ");
-                numeroEhValido = int.TryParse(Console.ReadLine(), out tamanhoDoDiamante);
-
-                Console.WriteLine("---------------------------------------------");
-
-                if (tamanhoDoDiamante <= 1 || tamanhoDoDiamante > 99 || tamanhoDoDiamante % 2 == 0)
+                do
                 {
-                    numeroEhValido = false;
-                    Console.WriteLine("Por favor, digite um número impar maior que 1 e menor que 100!");
+                    Console.Clear();
+                    Console.WriteLine("---------------------------------------------");
+                    Console.WriteLine("Diamante de X");
                     Console.WriteLine("---------------------------------------------");
 
-                    Console.ReadLine();
+                    Console.Write("Digite um número ímpar positivo: ");
+                    numeroEhValido = int.TryParse(Console.ReadLine(), out tamanhoDoDiamante);
+
+                    Console.WriteLine("---------------------------------------------");
+
+                    if (tamanhoDoDiamante <= 1 || tamanhoDoDiamante > 99 || tamanhoDoDiamante % 2 == 0)
+                    {
+                        numeroEhValido = false;
+                        Console.WriteLine("Por favor, digite um número ímpar maior que 1 e menor que 100!");
+                        Console.WriteLine("---------------------------------------------");
+
+                        Console.ReadLine();
+                    }
+                } while (numeroEhValido == false);
+
+                int quantidadeDeLinhas = (tamanhoDoDiamante - 1) / 2;
+                int quantidadeDeEspacos = (tamanhoDoDiamante - 1) / 2;
+                int quantidadeDeX = 1;
+
+                // Desenho da parte superior
+                for (int linha = 0; linha < quantidadeDeLinhas; linha++)
+                {
+                    for (int espacos = 0; espacos < quantidadeDeEspacos; espacos++)
+                        Console.Write(" ");
+
+                    for (int x = 0; x < quantidadeDeX; x++)
+                        Console.Write("x");
+
+                    quantidadeDeEspacos--;
+                    quantidadeDeX += 2;
+
+                    Console.WriteLine();
                 }
-            } while (numeroEhValido == false);
 
-            int quantidadeDeLinhas = (tamanhoDoDiamante - 1) / 2;
-            int quantidadeDeEspacos = (tamanhoDoDiamante - 1) / 2;
-            int quantidadeDeX = 1;
-
-            // Desenho da parte superior
-            for (int linha = 0; linha < quantidadeDeLinhas; linha++)
-            {
-                for (int espacos = 0; espacos < quantidadeDeEspacos; espacos++)
-                    Console.Write(" ");
-
-                for (int x = 0; x < quantidadeDeX; x++)
+                // Desenho da parte do meio
+                for (int coluna = 0; coluna < tamanhoDoDiamante; coluna++)
                     Console.Write("x");
-
-                quantidadeDeEspacos--;
-                quantidadeDeX += 2;
 
                 Console.WriteLine();
-            }
 
-            // Desenho da parte do meio
-            for (int coluna = 0; coluna < tamanhoDoDiamante; coluna++)
-                Console.Write("x");
-
-            Console.WriteLine();
-
-            // Desenho da parte inferior
-            quantidadeDeEspacos = 1;
-            quantidadeDeX -= 2;
-
-            for (int linha = 0; linha < quantidadeDeLinhas; linha++)
-            {
-                for (int espacos = 0; espacos < quantidadeDeEspacos; espacos++)
-                    Console.Write(" ");
-
-                for (int x = 0; x < quantidadeDeX; x++)
-                    Console.Write("x");
-
-                quantidadeDeEspacos++;
+                // Desenho da parte inferior
+                quantidadeDeEspacos = 1;
                 quantidadeDeX -= 2;
 
-                Console.WriteLine();
-            }
+                for (int linha = 0; linha < quantidadeDeLinhas; linha++)
+                {
+                    for (int espacos = 0; espacos < quantidadeDeEspacos; espacos++)
+                        Console.Write(" ");
 
-            Console.ReadLine();
+                    for (int x = 0; x < quantidadeDeX; x++)
+                        Console.Write("x");
+
+                    quantidadeDeEspacos++;
+                    quantidadeDeX -= 2;
+
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("---------------------------------------------");
+                Console.Write("Deseja desenhar outro diamante? s/N ");
+
+                if (Console.ReadLine()?.ToUpper() != "S")
+                    break;
+            }
+            
         }
     }
 }
